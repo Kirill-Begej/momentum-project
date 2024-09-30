3b492a12f2b2203762bc0b2736f67a8b05bf9cb4
Kirill Begej <fleki679@yandex.ru>
2024-09-30 17:00:49 +0300

    feat: настроить конфигурацию webpack

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    requireConfigFile: false,
+    babelOptions: {
+      babelrc: false,
+      configFile: false,
+      presets: ['@babel/preset-env'],
+    },
+  },
+  extends: ['airbnb-base'],
+  rules: {
+    'no-undef': 0,
+    'no-console': 0,
+    'import/prefer-default-export': 0,
+    'class-methods-use-this': 0,
+    'no-underscore-dangle': 0,
+    'no-param-reassign': 0,
+    'no-useless-constructor': 0,
+    'no-use-before-define': 0,
+    'no-else-return': 0,
+    'consistent-return': 0,
+  },
+};
