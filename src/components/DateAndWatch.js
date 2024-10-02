b6742e82ef35bb81afb6cfac98c921bbacd5eb87
Kirill Begey <kirill.begey@mail.ru>
2024-10-02 23:49:07 +0300

    feat: добавить логику отображения даты

--- src/components/DateAndWatch.js
+++ src/components/DateAndWatch.js
@@ -3,14 +3,27 @@ export default class DateAndWatch {
     this._hoursElement = document.querySelector('#hours');
     this._minutesElement = document.querySelector('#minutes');
     this._secondsElement = document.querySelector('#seconds');
+    this._dateElement = document.querySelector('#date');
+    this._monthsOfTheYear = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+    this._dayOfTheWeek = ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'];
   }
 
   enableDateAndWatch() {
-    this._enableWatch();
+    this._date = new Date();
+    this._intervalId = setInterval(() => {
+      this._date = new Date();
+      this._getWatch();
+      this._setTimeInHtml();
+      this._getDate();
+      this._setDateInHtml();
+    }, 1000);
+    this._getWatch();
+    this._setTimeInHtml();
+    this._getDate();
+    this._setDateInHtml();
   }
 
-  _getTime() {
-    this._date = new Date();
+  _getWatch() {
     this._hours = this._date.getHours();
     if (this._hours < 10) this._hours = `0${this._hours}`;
     this._minutes = this._date.getMinutes();
@@ -25,12 +38,13 @@ export default class DateAndWatch {
     this._secondsElement.textContent = this._seconds;
   }
 
-  _enableWatch() {
-    this._dateIntervalId = setInterval(() => {
-      this._getTime();
-      this._setTimeInHtml();
-    }, 1000);
-    this._getTime();
-    this._setTimeInHtml();
+  _getDate() {
+    this._dayOfTheMonth = this._date.getDate();
+    this._monthName = this._monthsOfTheYear[this._date.getMonth()];
+    this._dayName = this._dayOfTheWeek[this._date.getDay()];
+  }
+
+  _setDateInHtml() {
+    this._dateElement.textContent = `${this._dayOfTheMonth} ${this._monthName}, ${this._dayName}`;
   }
 }
